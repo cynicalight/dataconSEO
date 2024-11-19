45381945957323690337e89094a0ded419268d25
jz <391900788@qq.com>
2024-11-19 16:19:19 +0800

    fill the contents of three blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,5 @@
 import SingleBlog from "@/components/Blog/SingleBlog";
 import blogData from "@/components/Blog/blogData";
-import blogData2 from "@/components/Blog/blogData2";
-import blogData3 from "@/components/Blog/blogData3";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -13,9 +11,6 @@ export const metadata: Metadata = {
 };
 
 const Blog = () => {
-  // 合并所有的 blogData
-  const allBlogData = [...blogData, ...blogData1, ...blogData2];
-
   return (
     <>
       <Breadcrumb
@@ -26,7 +21,7 @@ const Blog = () => {
       <section className="pb-[120px] pt-[120px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center">
-            {allBlogData.map((blog) => (
+            {blogData.map((blog) => (
               <div
                 key={blog.id}
                 className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
